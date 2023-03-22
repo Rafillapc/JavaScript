db5b870e77b9ecc8887deb0e6860de71cfd83020
Rafa Paradas <114488909+Rafillapc@users.noreply.github.com>
2023-03-22 12:51:43 +0100

    Add files via upload

---
+++ ejercicio-modulos/README.md
@@ -0,0 +1,2 @@
+# Ejercicio-modulos-JS
+ 
---
+++ ejercicio-modulos/index.js
@@ -0,0 +1,7 @@
+import {suma, multiplica} from './modulosES/controller.js'
+
+const sum = suma(1, 2);
+console.log(sum)
+
+const multi = multiplica(4, 5);
+console.log(multi)
