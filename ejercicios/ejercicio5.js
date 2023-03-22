de15577c95b3ea48664968685f134a0444c46a17
Rafa Paradas <114488909+Rafillapc@users.noreply.github.com>
2023-03-22 12:55:57 +0100

    Create ejercicio5.js

---
+++ ejercicios/ejercicio5.js
@@ -0,0 +1,8 @@
+let altura = 163;
+let alturaM = 1.63;
+let peso = 73.2;
+let alturaR = alturaM.toFixed(3);
+console.log(alturaR);
+
+max_valor_JS = Number.MAX_VALUE + 1;
+max_valor_JS2 =max_valor_JS;
