e44a69edf775495681cd2bac04152a0223f6b7e6
Rafa Paradas <114488909+Rafillapc@users.noreply.github.com>
2023-03-22 12:55:40 +0100

    Create ejercicio4.js

---
+++ ejercicios/ejercicio4.js
@@ -0,0 +1,19 @@
+let nombre = "Rafa";
+
+let apellido = "Paradas"
+
+let estudiante = `${nombre} ${apellido}`;
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let numeroPalabras = estudiante.length;
+
+let primeraLetra = nombre.slice(0,1);
+
+let ultimaLetra = nombre.slice(3, 4);
+
+let sinEspacios = estudiante.replace(" ", "");
+
+let ifName = estudiante.startsWith("Rafa");
